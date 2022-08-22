cfca7a58462628b2891dbaa659a077eaf58a6f4a
mikseros <mmadynski@gmail.com>
2022-08-22 20:46:29 +0200

    Eloquent model (php artisan make:model Listing)

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,33 +1,11 @@
 <?php
-    namespace App\Models;
 
-    class Listing {
-        public static function all() {
-            return [
-                [
-                    'id' => 1,
-                    'title' => 'Listing One',
-                    'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                                      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                                      Arcu felis bibendum ut tristique et egestas quis ipsum suspendisse.
-                                      Ullamcorper sit amet risus nullam eget felis. Ultrices mi tempus imperdiet nulla malesuada pellentesque elit eget.
-                                      Auctor eu augue ut lectus arcu. Habitant morbi tristique senectus et netus et malesuada fames ac.'
-                ],
-                [
-                    'id' => 2,
-                    'title' => 'Listing Two',
-                    'description' => 'something else'
-                ]
-            ];
-        }
+namespace App\Models;
 
-        public static function find($id) {
-            $listings = self::all();
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-            foreach($listings as $listing) {
-                if($listing['id'] == $id) {
-                    return $listing;
-                }
-            }
-        }
-    }
+class Listing extends Model
+{
+    use HasFactory;
+}
