2783de7f0d77dfebe106bbb1967f211b2784fe27
mikseros <mmadynski@gmail.com>
2022-08-23 21:18:19 +0200

    Template Partials

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,6 +1,8 @@
 @extends('layout')
 
 @section('content')
+@include('partials._hero')
+@include('partials._search')
 
 <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
 
@@ -9,7 +11,7 @@
     @endif
     @foreach($listings as $listing)
         <div class="bg-gray-50 border border-gray-200 rounded p-6">
-            <div class="flex">
+            <div class="inline">
                 <img
                     class="hidden w-48 mr-6 md:block"
                     src="{{asset('images/no-image.png')}}"
@@ -17,7 +19,7 @@
                 />
                 <div>
                     <h3 class="text-2xl">
-                        <a href="show.html">{{$listing->title}}</a>
+                        <a href="/listings/{{$listing->id}}">{{$listing->title}}</a>
                     </h3>
                     <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
                     <ul class="flex">
