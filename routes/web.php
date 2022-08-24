cd2f51f81bcf7032864df61e1ac1a69aa7f85042
mikseros <mmadynski@gmail.com>
2022-08-24 22:35:39 +0200

    Listing form created

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,9 @@ use App\Models\Listing;
 // All Listings
 Route::get('/', [ListingController::class, 'index']);
 
+// Show create form
+Route::get('/listings/create', [ListingController::class, 'create']);
+
 // Single Listing
 
 // Route::get('/listings/{id}', function($id) {
