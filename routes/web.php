b9cab98c8f7bc0141f6467c1c3bce73d8feb1ca8
mikseros <mmadynski@gmail.com>
2022-08-24 19:34:51 +0200

    Controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ListingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Listing;
@@ -15,12 +16,7 @@ use App\Models\Listing;
 |
 */
 // All Listings
-Route::get('/', function () {
-    return view('listings', [
-        'heading' => 'Latest Listings',
-        'listings' => Listing::all()
-    ]);
-});
+Route::get('/', [ListingController::class, 'index']);
 
 // Single Listing
 
@@ -36,8 +32,4 @@ Route::get('/', function () {
 //     }
 // });
 // Code below has automatically '404 functionality'
-Route::get('/listings/{listing}', function(Listing $listing) {
-    return view('listing', [
-        'listing' => $listing
-    ]);
-});
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
