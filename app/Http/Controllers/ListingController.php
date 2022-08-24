28ee7e2d42d8d29f0cc6fac1d524fd3abbeb4109
mikseros <mmadynski@gmail.com>
2022-08-24 21:51:34 +0200

    Tag Filter

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -9,8 +9,9 @@ class ListingController extends Controller
 {
     // Show all listings
     public function index() {
+        // dd(request('tag'));
         return view('listings.index', [
-            'listings' => Listing::all()
+            'listings' => Listing::latest()->filter(request(['tag']))->get()
         ]);
     }
 
