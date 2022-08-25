4d77659d11bd75ace5b0cb7dd071f4d203d23936
mikseros <mmadynski@gmail.com>
2022-08-25 12:39:43 +0200

    Validation & Store data from Listing form

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Listing;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class ListingController extends Controller
 {
@@ -25,5 +26,22 @@ class ListingController extends Controller
     // Show create form
     public function create() {
         return view('listings.create');
+    }
+
+    // Store Listing Data
+    public function store(Request $request) {
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => ['required', Rule::unique('listings', 'company')],
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required', 'email'],
+            'tags' => 'required',
+            'description' => 'required'
+        ]);
+
+        Listing::create($formFields);
+        
+        return redirect('/');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,9 @@ Route::get('/', [ListingController::class, 'index']);
 // Show create form
 Route::get('/listings/create', [ListingController::class, 'create']);
 
+// Store Listing Data
+Route::post('/listings', [ListingController::class, 'store']);
+
 // Single Listing
 
 // Route::get('/listings/{id}', function($id) {
