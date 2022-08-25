f3b381aa4fc192ace49793b49c2bb015458ad2a4
mikseros <mmadynski@gmail.com>
2022-08-25 12:51:36 +0200

    Flash Messages

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -41,7 +41,7 @@ class ListingController extends Controller
         ]);
 
         Listing::create($formFields);
-        
-        return redirect('/');
+
+        return redirect('/')->with('message', 'Listing created successfully!');
     }
 }
