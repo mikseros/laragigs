9d88f67c89a3f29b3c09d0498f0689061ac76a4f
mikseros <mmadynski@gmail.com>
2022-08-26 15:22:40 +0200

    Edit Listing

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -8,21 +8,21 @@ use Illuminate\Validation\Rule;
 
 class ListingController extends Controller
 {
-    // Show all listings
+    // Show All Listings
     public function index() {
         return view('listings.index', [
             'listings' => Listing::latest()->filter(request(['tag', 'search']))->paginate(6)
         ]);
     }
 
-    // Show single listing
+    // Show Single Listing
     public function show(Listing $listing) {
         return view('listings.show', [
             'listing' => $listing
         ]);
     }
 
-    // Show create form
+    // Show Create Form
     public function create() {
         return view('listings.create');
     }
@@ -46,5 +46,31 @@ class ListingController extends Controller
         Listing::create($formFields);
 
         return redirect('/')->with('message', 'Listing created successfully!');
+    }
+
+    // Show Edit Form
+    public function edit(Listing $listing) {
+        return view('listings.edit', ['listing' => $listing]);
+    }
+
+    // Update Listing Data
+    public function update(Request $request, Listing $listing) {
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => 'required',
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required', 'email'],
+            'tags' => 'required',
+            'description' => 'required'
+        ]);
+
+        if($request->hasFile('logo')) {
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+        }
+
+        $listing->update($formFields);
+
+        return back()->with('message', 'Listing updated successfully!');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,17 @@ use App\Models\Listing;
 // All Listings
 Route::get('/', [ListingController::class, 'index']);
 
-// Show create form
+// Show Create Form
 Route::get('/listings/create', [ListingController::class, 'create']);
 
 // Store Listing Data
 Route::post('/listings', [ListingController::class, 'store']);
+
+// Show Edit Form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
+
+// Update Listing
+Route::put('/listings/{listing}', [ListingController::class, 'update']);
 
 // Single Listing
 
